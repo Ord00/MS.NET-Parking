e3364297c6530d268e7747b40363adfa19d8a492
Симонов Иван <ivansimonov7@yandex.ru>
2024-11-12 18:13:31 +0300

    Ended Vehicle model

---
+++ Parking/Parking.BL/Mapper/VehicleModelsProfile.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using Parking.BL.Vehicles.Entities;
+
+namespace Parking.BL.Mapper;
+
+public class VehicleModelsProfile : Profile
+{
+    public VehicleModelsProfile()
+    {
+        CreateMap<VehicleModel, VehicleResponse>().ReverseMap()
+            .ForMember(x => x.Id, y => y.MapFrom(z => z.Id))
+            .ForMember(x => x.Brand, y => y.MapFrom(z => z.Brand))
+            .ForMember(x => x.Model, y => y.MapFrom(z => z.Model))
+            .ForMember(x => x.Colour, y => y.MapFrom(z => z.Colour))
+            .ForMember(x => x.RegistrationPlateId, y => y.MapFrom(z => z.RegistrationPlateId))
+            .ForMember(x => x.VehicleTypeId, y => y.MapFrom(z => z.VehicleTypeId));
+    }
+}
