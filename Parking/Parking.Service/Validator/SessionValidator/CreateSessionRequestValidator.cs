61c798f7ac0af52ac5ef77684fac213afd23f94c
Симонов Иван <ivansimonov7@yandex.ru>
2024-11-12 21:39:18 +0300

    Added session validation

--- Parking/Parking.Service/Validator/SessionValidator/CreateSessionRequestValidator.cs
+++ Parking/Parking.Service/Validator/SessionValidator/CreateSessionRequestValidator.cs
@@ -1,6 +1,28 @@
+using FluentValidation;
+using Parking.Controllers.SessionController;
+
 namespace Parking.Validator.SessionValidator;
 
-public class CreateSessionRequestValidator
+public class CreateSessionRequestValidator : AbstractValidator<CreateSessionRequest>
 {
-    
+    public CreateSessionRequestValidator()
+    {
+        RuleFor(x => x.EntryDate)
+            .NotEmpty()
+            .Must(y => y <= DateTime.UtcNow)
+            .WithMessage("Entry date is required");
+        
+        RuleFor(x => x.ExitDate)
+            .NotEmpty()
+            .Must(y => y > DateTime.UtcNow)
+            .WithMessage("Exit date is required");
+        
+        RuleFor(x => x.UserId)
+            .NotEmpty()
+            .WithMessage("User id is required");
+        
+        RuleFor(x => x.VehicleId)
+            .NotEmpty()
+            .WithMessage("Vehicle id is required");
+    }
 }
